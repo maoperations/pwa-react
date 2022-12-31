0e5ee7c61a0340244600fed3c69de4eb4147933e
Juki Beng Hantachi <jukibenghantachi@gmail.com>
2022-12-31 16:25:39 +0700

    feat: styling modal

--- src/components/Modals/index.jsx
+++ src/components/Modals/index.jsx
@@ -1,44 +1,120 @@
-import Sheet from 'react-modal-sheet'
-import styled from 'styled-components'
-import { TYPES_COLOR } from '../../constant/color'
-import { LBS, FEET } from '../../constant/calculate'
-import useSpecies from '../../hooks/useSpecies'
+import Sheet from "react-modal-sheet";
+import styled from "styled-components";
+import { TYPES_COLOR, X_COLOR } from "../../constant/color";
+import { LBS, FEET } from "../../constant/calculate";
+import useSpecies from "../../hooks/useSpecies";
 
 const Modals = ({ show, onClose, data }) => {
-  if (!data) return null
-  const { data: spesies } = useSpecies(data.id)
-  const color = TYPES_COLOR[data.types[0].type.name]
+  if (!data) return null;
+  const { data: spesies } = useSpecies(data.id);
+  const color = TYPES_COLOR[data.types[0].type.name];
+  const padId = data.id.toString().padStart(3, 0);
 
-  console.log(spesies)
+  console.log(spesies);
   return (
     <>
-      <Sheet rootId='root' isOpen={show} onClose={onClose}>
+      <Sheet rootId="root" isOpen={show} onClose={onClose}>
         <Sheet.Container style={{ backgroundColor: color }}>
-          <Sheet.Header />
-          <Sheet.Content>
-            <div className='flex items-center justify-between p-5'>
+          <Sheet.Header>
+            <div className="relative flex justify-center">
+              <div className="absolute -z-10">
+                <p className="outline-text uppercase opacity-50">{data.name}</p>
+              </div>
+
+              <div className="relative flex items-center justify-center gap-10 pt-28">
+                <div className="absolute right-60 -z-10">
+                  <img src="/Pokeball.svg" className="w-48 h-48 opacity-10" />
+                </div>
+
+                <div className="absolute -right-20">
+                  <img src="/Pattern.svg" className="w-32 h-32 opacity-80" />
+                </div>
+
+                <img
+                  src={data.sprites.other["official-artwork"].front_default}
+                  className="w-36 h-36"
+                />
+                <div>
+                  <h1 className="text-[rgba(23,23,27,0.6)] font-bold">{`#${padId}`}</h1>
+                  <h2 className="text-white text-3xl font-bold capitalize">
+                    {data.name}
+                  </h2>
+                  {data.types.map((type, i) => (
+                    <button
+                      key={i}
+                      className="text-sm mt-2 mr-2 px-4 py-2 text-white rounded-lg tracking-wider outline-none cursor-default"
+                      style={{ backgroundColor: X_COLOR[type.type.name].color }}
+                    >
+                      <div className="flex justify-between">
+                        <img
+                          src={X_COLOR[type.type.name].btn}
+                          className="flex mr-2"
+                        />
+                        <p className="capitalize font-medium">
+                          {type.type.name}
+                        </p>
+                      </div>
+                    </button>
+                  ))}
+                </div>
+              </div>
+            </div>
+
+            <div className="flex items-center justify-between p-5">
               <p>About</p>
               <p>Stats</p>
               <p>Evolution</p>
             </div>
-
-            <div className='block'>
-              <p>{data.name}</p>
-              <p>Pokédex Data</p>
-              <p>{data.species.name}</p>
-              <p>{(data.height / 10).toFixed(1)} height</p>
-              <p>{(data.weight / 10).toFixed(1)} weight</p>
-              <p>{LBS((data.weight / 10).toFixed(1))} lbs</p>
-              <p>{FEET((data.height / 10).toFixed(1))}</p>
-              {data.abilities.map((data, i) =>
-                i > 0 ? (
-                  <p key={i}>{data.ability.name}(hidden ability)</p>
-                ) : (
-                  <p key={i}>
-                    {i + 1}.{data.ability.name}
-                  </p>
-                )
-              )}
+          </Sheet.Header>
+          <Sheet.Content className="bg-white rounded-t-3xl p-5">
+            <div className="">
+              <p className="font-semibold text-xl" style={{ color: color }}>
+                Pokédex Data
+              </p>
+              <table className="table-auto w-1/2 border-separate border-spacing-y-4">
+                <tbody>
+                  <tr>
+                    <td className="font-medium">Species</td>
+                    <td>{spesies?.genera[7].genus}</td>
+                  </tr>
+                  <tr>
+                    <td className="font-medium">Height</td>
+                    <td>
+                      {(data.height / 10).toFixed(1)}m{" "}
+                      {FEET((data.height / 10).toFixed(1))}
+                    </td>
+                  </tr>
+                  <tr>
+                    <td className="font-medium">Weight</td>
+                    <td>
+                      {(data.weight / 10).toFixed(1)}kg{" "}
+                      {LBS((data.weight / 10).toFixed(1))} lbs
+                    </td>
+                  </tr>
+                  <tr>
+                    <td className="font-medium">Abilities</td>
+                    <td>
+                      {data.abilities.map((data, i) =>
+                        i > 0 ? (
+                          <p key={i} className="capitalize">
+                            {data.ability.name}
+                            <span className="normal-case">
+                              (hidden ability)
+                            </span>
+                          </p>
+                        ) : (
+                          <span key={i}>
+                            {i + 1}.{" "}
+                            <span className="capitalize">
+                              {data.ability.name}
+                            </span>
+                          </span>
+                        )
+                      )}
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
               <p>{data.base_experience}</p>
               <p>{spesies?.base_happiness}</p>
               <p>{spesies?.capture_rate}</p>
@@ -53,8 +129,8 @@ const Modals = ({ show, onClose, data }) => {
         <Sheet.Backdrop />
       </Sheet>
     </>
-  )
-}
+  );
+};
 
 const BoxList = styled.div`
   height: 100%;
@@ -63,7 +139,7 @@ const BoxList = styled.div`
   padding: 16px;
   padding-top: 0px;
   overflow: auto;
-`
+`;
 
 const Box = styled.div`
   background-color: #eee;
@@ -75,6 +151,6 @@ const Box = styled.div`
   justify-content: center;
   font-weight: 700;
   font-size: 24px;
-`
+`;
 
-export default Modals
+export default Modals;
